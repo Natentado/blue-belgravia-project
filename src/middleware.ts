45ec7f6ecc1ae461f2df76f735a63064f8b5c4b1
Nathan Molina <nathan.molina@chavesnamao.com.br>
2024-03-23 17:25:15 -0300

    project initialization

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse, userAgent } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    const url = request.nextUrl;
+    const { device } = userAgent(request);
+    const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
+    url.searchParams.set('viewport', viewport);
+    
+    return NextResponse.rewrite(url);
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: '/:path*',
+};
