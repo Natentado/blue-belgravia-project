4bd22795989172b89bda7cc2f51202b2b32bb5e3
Nathan Molina <nathan.molina@chavesnamao.com.br>
2024-03-24 00:42:01 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 import path from 'path';
 
+/**
+ * @type {import('next').NextConfig}
+*/
+
 export const nextConfig = {
   sassOptions: {
     includePaths: [path.join(process.cwd(), 'styles')],
   },
 };
+
+// module.exports = nextConfig
