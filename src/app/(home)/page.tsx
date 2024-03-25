11bf1232f1977ef4ba6f768ee4e3012b29f59cfb
Nathan Molina <nathan.molina@chavesnamao.com.br>
2024-03-25 00:07:05 -0300

    feat: join team section mobile UI

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,6 +6,8 @@ import RestaurantIcon from "@public/assets/img/icons/restaurant.png";
 import BuffetIcon from "@public/assets/img/icons/buffet.png";
 import PartyIcon from "@public/assets/img/icons/party.png";
 import ChampagneIcon from "@public/assets/img/icons/champagne.png";
+import employersImg from "@public/assets/img/employers.png";
+import candidatesImg from "@public/assets/img/candidates.png";
 import Link from "next/link";
 
 interface PageParams {
@@ -104,6 +106,70 @@ const HomePage = (props: PageParams) => {
           </p>
         </span>
       </section>
+      <section className={styles.purpose}>
+        <h2>
+          Bar Staff, Waiter and 
+          Hostess Recruitment in <b>Central London</b>
+        </h2>
+        <p>
+          Transform the face of your event with 
+          {" "} <b>professional hosting</b> <br/> 
+          from our expert staff.
+        </p>
+      </section>
+      <article className={styles.joinTeamSections}>
+        <section>
+          <h2>For Employers</h2>
+          <small />
+
+          <div>
+            <span>
+              <p>Count on our impeccably employees today.</p>
+              <small>
+                With more than 20 years of experience in hosting, 
+                we know how to deliver.
+              </small>
+
+              <button>Hire candidates</button>
+            </span>
+
+            <picture>
+              <Image 
+                alt=" for employers section image"
+                src={employersImg}
+                width={180}
+                height={180}
+              />
+            </picture>
+          </div>
+        </section>
+        
+        <section>
+          <h2>For Candidates</h2>
+          <small />
+
+          <div>
+            <span>
+              <p>Enhance your skills by joining our team.</p>
+              <small>
+                We offer the finest training, and the opportunity to 
+                work in the vibrant hospitality industry of London.
+              </small>
+
+              <button>Apply now!</button>
+            </span>
+
+            <picture>
+              <Image 
+                alt=" for employers section image"
+                src={candidatesImg}
+                width={180}
+                height={180}
+              />
+            </picture>
+          </div>
+        </section>
+      </article>
     </main>
   );
 };
