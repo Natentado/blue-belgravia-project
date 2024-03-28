9253b9131904cea351ca4380ade15ae7aa76e1f0
Nathan Molina <nathan.molina@chavesnamao.com.br>
2024-03-28 20:56:39 -0300

    github account test

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,9 @@ import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const url = request.nextUrl;
     const { device } = userAgent(request);
     const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
+    const url = request.nextUrl;
     url.searchParams.set('viewport', viewport);
     
     return NextResponse.rewrite(url);
